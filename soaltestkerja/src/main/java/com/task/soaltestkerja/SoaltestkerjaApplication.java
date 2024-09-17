91f5cf5ba5151cefd4ada288acfc14203cc78977
Robi Nurhidayat <robivanpersie290899@gmail.com>
2024-09-17 20:05:31 +0700

    add starter project

---
+++ soaltestkerja/src/main/java/com/task/soaltestkerja/SoaltestkerjaApplication.java
@@ -0,0 +1,13 @@
+package com.task.soaltestkerja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaltestkerjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaltestkerjaApplication.class, args);
+	}
+
+}
---
+++ soaltestkerja/src/test/java/com/task/soaltestkerja/SoaltestkerjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.soaltestkerja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaltestkerjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
